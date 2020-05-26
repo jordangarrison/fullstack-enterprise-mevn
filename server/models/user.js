011642905a0d19fd5b8e117943eeba186467f5ec
Jordan Garrison <jordan.andrew.garrison@gmail.com>
2020-05-25 21:27:40 -0500

    Hash user password and generate email conf code

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import Bcrypt from 'bcryptjs'
+import randomstring from 'randomstring'
 
 const UserSchema = new mongoose.Schema({
   name: String,
@@ -7,7 +9,14 @@ const UserSchema = new mongoose.Schema({
   updatedAt: Date,
   password: String,
   emailConfirmedAt: Date,
-  emailConfirmedCode: String
+  emailConfirmCode: String
+})
+
+UserSchema.pre('save', function () {
+  this.password = Bcrypt.hashSync(this.password)
+  this.emailConfirmCode = randomstring.generate(72)
+
+  this.createdAt = new Date()
 })
 
 export default mongoose.model('User', UserSchema)
