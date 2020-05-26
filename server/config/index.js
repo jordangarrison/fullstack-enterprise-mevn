14cf27e9f8b0064520958b7a402667fd3e2799cb
Jordan Garrison <jordan.andrew.garrison@gmail.com>
2020-05-25 22:12:22 -0500

    Sending confirmation emails

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv'
 
 dotenv.config()
-
+const databaseUri =
+  process.env.MEVN_DATABASE_URL || 'mongodb://localhost:12345/mevn'
+const serverPort = process.env.MEVN_SERVER_PORT || 7000
+const appUrl = process.env.MEVN_APP_URL || `http://localhost:${serverPort}`
 export default {
-  databaseUri:
-    process.env.MEVN_DATABASE_URL || 'mongodb://localhost:12345/mevn',
-  serverPort: process.env.MEVN_SERVER_PORT || 7000
+  databaseUri: databaseUri,
+  serverPort: serverPort,
+  appUrl: appUrl
 }
--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,8 @@
+import config from '@config'
 import mongoose from 'mongoose'
 import Bcrypt from 'bcryptjs'
 import randomstring from 'randomstring'
+import Mail from '@fullstackjs/mail'
 
 const UserSchema = new mongoose.Schema({
   name: String,
@@ -17,6 +19,17 @@ UserSchema.pre('save', function () {
   this.emailConfirmCode = randomstring.generate(72)
 
   this.createdAt = new Date()
+})
+
+UserSchema.post('save', function () {
+  new Mail('confirm-account')
+    .to(this.email, this.name)
+    .subject('From MEVN Team: Please confirm your account')
+    .data({
+      name: this.name,
+      url: `${config.appUrl}/auth/emails/confirm/${this.emailConfirmCode}`
+    })
+    .send()
 })
 
 export default mongoose.model('User', UserSchema)
